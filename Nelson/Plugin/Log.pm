83aa7038de78a9b557fd7cf16851e6ee2b471309
Jonas Kramer <jkramer@nex.scrapping.cc>
2010-01-14 20:06:25 +0100

    Added log plugin.

---
+++ Nelson/Plugin/Log.pm
@@ -0,0 +1,46 @@
+
+package Nelson::Plugin::Log;
+
+use strict;
+use warnings;
+
+use base qw( Nelson::Plugin );
+
+use IO::File;
+
+
+sub namespace { 'log' }
+
+
+sub priority { 1000 }
+
+
+sub initialize {
+	my ($self, $nelson, %cfg) = @_;
+
+	if($cfg{path}) {
+		$self->{log_file} = new IO::File($cfg{path}, '>>');
+		$self->{log_file}->autoflush(1);
+	}
+	else {
+		warn "Log plugin loaded, but no log path configured.\n";
+	}
+}
+
+
+sub message {
+	my ($self, $message) = @_;
+
+	if($self->{log_file}) {
+		my $from = $message->from;
+		my $text = $message->text;
+		my $time = localtime;
+
+		$self->{log_file}->write("$time $from: $text\n");
+	}
+
+	return 1;
+}
+
+
+1
