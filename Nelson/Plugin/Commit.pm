74b5b1fc3a6d3861134b1d09ed9fce8f26bbd9fa
Jonas Kramer <jkramer@nex.scrapping.cc>
2010-12-31 15:14:57 +0100

    Added what-the-commit plugin.

---
+++ Nelson/Plugin/Commit.pm
@@ -0,0 +1,52 @@
+
+package Nelson::Plugin::Commit;
+
+use strict;
+use warnings;
+
+use base qw( Nelson::Plugin );
+
+use WWW::Mechanize;
+use Data::Dumper;
+
+
+sub namespace { 'commit' }
+
+
+sub priority { 1002 }
+
+
+sub message {
+	my ($self, $message) = @_;
+
+	if($message->text =~ m/^!commit\s*$/i) {
+
+		my $url = "http://whatthecommit.com/";
+
+		my $result = $self->mechanize->get($url);
+
+		if(defined($result) and length($result)) {
+			my $content = $self->mechanize->content;
+			$content =~ s#.*<p>(.*)</p>.*#$1#gmsi;
+
+			$message->reply($content);
+		}
+		else {
+			$message->reply("Nop! You stink!");
+		}
+	}
+
+	return 1;
+}
+
+
+sub mechanize {
+    my ($self) = @_;
+
+    $self->{_mechanize} ||= new WWW::Mechanize(autocheck => 0);
+
+    return $self->{_mechanize};
+}
+
+
+1
