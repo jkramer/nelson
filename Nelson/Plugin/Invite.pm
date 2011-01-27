260b9a6a4113bce8dc48d29440d1d455d153f76a
Jonas Kramer <jkramer@nex.scrapping.cc>
2011-01-28 02:00:11 +0800

    Make list of channels (for invitations) configurable.

--- Nelson/Plugin/Invite.pm
+++ Nelson/Plugin/Invite.pm
@@ -12,11 +12,27 @@ sub namespace { 'invite' }
 sub priority { 100 }
 
 
+sub initialize {
+	my ($self, $nelson, %cfg) = @_;
+
+	$self->{channel_list} = [ split /\s*,\s*/, ($cfg{channels} || '') ];
+
+	s/^(\W)/#$1/ for(@{$self->{channel_list}});
+
+	return $self;
+}
+
+
 # Ignore everything that is not a nelson command (e.g. starts with '!').
 sub invite {
 	my ($self, $message) = @_;
 
-	$message->connection->join($message->text);
+	my $channel = $message->text;
+	my @channel_list = @{$self->{channel_list}};
+
+	if(@channel_list && grep { $_ eq $channel } @channel_list) {
+		$message->connection->join($message->text);
+	}
 
 	return 1;
 }
